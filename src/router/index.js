354a9f1d0444685628b783c1542f5bff3ecf04cc
tazz0009 <tazz0009@gmail.com>
2021-01-02 08:00:13 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Todo from "../views/Todo";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Todo",
+    component: Todo
   },
   {
     path: "/about",
@@ -17,7 +17,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About")
   }
 ];
 
